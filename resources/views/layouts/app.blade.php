7084f27e8788495468c2fa361b0de0c7160c85a6
Amit <amit@AZ-iMac.local>
2018-09-01 17:58:01 -0400

    Added logic statement in app.blade so that Jumbotron only show on home page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,9 @@
     @include('inc.navbar')
 
     <div class="container">
-      @include('inc.showcase')
+      @if(Request::is('/'))
+        @include('inc.showcase')
+      @endif
       <div class="row">
         <div class="col-md-8 col-lg-8">
           @yield('content')
