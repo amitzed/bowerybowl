8137171a71f04d194e16781dbe77e662954d349c
Amit <amit@AZ-iMac.local>
2018-08-31 20:51:49 -0400

    Added to resources/views pages for home menu reservations

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/menu', function () {
+    return view('menu');
+});
+
+Route::get('/reservations', function () {
+    return view('reservations');
 });
