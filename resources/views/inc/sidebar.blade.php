b9e5a03290ba3536ba7f6b58a0e6477b919ef33d
Amit <amit@AZ-iMac.local>
2018-08-31 22:59:36 -0400

    Created include foler in views and moved sidebar DIV into new file created sidebar.blade.php

---
+++ resources/views/inc/sidebar.blade.php
@@ -0,0 +1,10 @@
+@section('sidebar')
+<div class="sidebar">
+  <h4>Hours of Operation:</h4>
+  Monday - Thursday: 11AM - 10PM
+  <br/>
+  Friday - Saturday: 11AM - 1AM
+  <br/>
+  Sunday: Closed
+  @show
+</div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,15 +6,6 @@
   </head>
   <body>
     @yield('content')
-    @section('sidebar')
-    <div class="sidebar">
-      <h4>Hours of Operation:</h4>
-      Monday - Thursday: 11AM - 10PM
-      <br/>
-      Friday - Saturday: 11AM - 1AM
-      <br/>
-      Sunday: Closed
-      @show
-    </div>
+    @include('inc.sidebar')
   </body>
 </html>
