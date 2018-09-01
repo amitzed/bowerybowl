0e10fa6128ad3bfbd3689b1eb6448ac95c023984
Amit <amit@AZ-iMac.local>
2018-09-01 00:29:08 -0400

    Added Nav bar and light styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,10 @@
     <meta charset="utf-8">
     <title>Bowery Bowl</title>
     <link rel="stylesheet" href="/css/app.css">
+    <link href="https://fonts.googleapis.com/css?family=Caveat|Gloria+Hallelujah|Indie+Flower|Lato|Permanent+Marker" rel="stylesheet">
   </head>
   <body>
+    @include('inc.navbar')
     @yield('content')
     @include('inc.sidebar')
   </body>
