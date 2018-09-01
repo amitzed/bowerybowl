92c0250673d2e59a37a3342e45a2958d35e072da
Amit <amit@AZ-iMac.local>
2018-09-01 02:52:12 -0400

    nav link menus word and redirect

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -8,13 +8,13 @@
       <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item active">
-            <a class="nav-link" href="#">Menu</a>
+            <a class="nav-link" href="/menu">Menu</a>
           </li>
           <li class="nav-item active">
-            <a class="nav-link" href="#">Reservations</a>
+            <a class="nav-link" href="/reservations">Reservations</a>
           </li>
           <!-- <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="https://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
