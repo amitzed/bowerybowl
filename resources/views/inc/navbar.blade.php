2d1c87ced2bb966de7f5d81e3e4f9cfa15b773c2
Amit <amit@AZ-iMac.local>
2018-09-01 01:49:10 -0400

    Added Logo and animation

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,5 +1,6 @@
 <nav class="navbar navbar-expand-md bg-dark">
-      <a class="navbar-brand" href="#">Bowery Bowl</a>
+  <link rel="icon" href="images/food1.jpg" />
+      <a class="navbar-brand" href="#">Bowery <img class="logo" src="{{ asset('images/logo2.jpg') }}"> Bowl</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -25,8 +26,8 @@
           </li> -->
         </ul>
         <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+          <input class="form-control mr-sm-2" type="text" placeholder="Search Ramen Recipes" aria-label="Search">
+          <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">GO</button>
         </form>
       </div>
     </nav>
