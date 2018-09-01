e35c4007d9a59263d7f17a96b9e8b5add571275d
Amit <amit@AZ-iMac.local>
2018-08-31 21:18:14 -0400

    added a side bar for store hours and appended a message to the side bar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,3 +3,8 @@
 @section('content')
   <h1>Home</h1>
 @endsection
+
+@section('sidebar')
+ @parent
+  <p>Join Us Weekdays 5PM-7PM for Happy Hour Discounts</p>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,5 +6,15 @@
   </head>
   <body>
     @yield('content')
+    @section('sidebar')
+    <div class="sidebar">
+      <h4>Hours of Operation:</h4>
+      Monday - Thursday: 11AM - 10PM
+      <br/>
+      Friday - Saturday: 11AM - 1AM
+      <br/>
+      Sunday: Closed
+      @show
+    </div>
   </body>
 </html>
